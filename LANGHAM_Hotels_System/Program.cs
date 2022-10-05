e87778142e10270666b3aad3c460dae648a32076
jes623 <112556590+jes623@users.noreply.github.com>
2022-10-06 02:49:10 +1300

    Add11

--- LANGHAM_Hotels_System/Program.cs
+++ LANGHAM_Hotels_System/Program.cs
@@ -265,8 +265,7 @@ namespace LANGHAM_Hotels_System
                     Console.WriteLine("The day you want for reservation : {0}", daymonthyear);
                     Console.WriteLine("The Checkout day : {0}", checkoutday);
                     Console.WriteLine("The current day : {0}", currentdaymonthyear);
-
-                    incorrect = string.Compare(currentdaymonthyear, daymonthyear);
+                    incorrect = DateTime.Compare(DateTime.Now, userinfo.DayMonthYear);
 
                     if (incorrect > 0 || incorrect == 0)
                     {
@@ -292,13 +291,14 @@ namespace LANGHAM_Hotels_System
                 if (reservationnumber == ListUserInfo[i].ReservationNumber)
                 {
                     found = true;
+                    break;
                 }
-                if (found)
-                {
-                    UserInfo result = ListUserInfo.Find(x => x.ReservationNumber == reservationnumber);
-                    ListUserInfo.Remove(result);
-                    ListRoomInfo.RemoveAt(i);
-                }
+                //if (found)
+                //{
+                //    UserInfo result = ListUserInfo.Find(x => x.ReservationNumber == reservationnumber);
+                //    ListUserInfo.Remove(result);
+                //    ListRoomInfo.RemoveAt(i);
+                //}
             }
             if (found)
             {
@@ -326,33 +326,46 @@ namespace LANGHAM_Hotels_System
         {
             try
             {
-                Console.WriteLine("If you want to check your reservation, please enter the reservation number you set");
+                Console.Write("If you want to check your reservation, please enter the reservation number you set : ");
                 int reservationnumber = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine();
 
                 DateTime now = DateTime.Now;
                 string currentdaymonthyear = now.ToString();
+                bool found = false;
                 for (int i = 0; i < ListUserInfo.Count; i++)
                 {
                     if (reservationnumber == ListUserInfo[i].ReservationNumber)
                     {
-
-                        Console.WriteLine("The Room Type : {0} / The Maximum People of the Room : {1} / " +
-                            "The Room Size : {2} / The Room Price : {3}$ / The Total Room Price : {4}$",
-                             ListRoomInfo[i].RoomType, ListRoomInfo[i].RoomPeople,
-                             ListRoomInfo[i].RoomSize, ListRoomInfo[i].RoomPrice, ListRoomInfo[i].RoomPrice * ListUserInfo[i].DayNumber);
-
-                        Console.WriteLine("Your Name : {0} / Your Phone Number : {1} / Your Requirement : {2}\n" +
-                            "Your Reservation Day(Check In) : {3} / The nights : {4} / Your Check Out Day : {5}\nThe current time : {6}",
-                            ListUserInfo[i].UserName, ListUserInfo[i].PhoneNumber, ListUserInfo[i].Requirement,
-                            ListUserInfo[i].DayMonthYear, ListUserInfo[i].DayNumber, ListUserInfo[i].CheckoutDay, currentdaymonthyear);
-                        Console.WriteLine();
+                        found = true;
+                        break;
                     }
-                    //else
-                    //{
-                    //    Console.WriteLine("Sorry your password is worng, please check it again Or reserve first and then make password");
-                    //}
                 }
+                if (found)
+                {
+                    for (int i = 0; i < ListUserInfo.Count; i++)
+                    {
+                        if (reservationnumber == ListUserInfo[i].ReservationNumber)
+                        {
+
+                            Console.WriteLine("The Room Type : {0} / The Maximum People of the Room : {1} / " +
+                                "The Room Size : {2} / The Room Price : {3}$ / The Total Room Price : {4}$",
+                                 ListRoomInfo[i].RoomType, ListRoomInfo[i].RoomPeople,
+                                 ListRoomInfo[i].RoomSize, ListRoomInfo[i].RoomPrice, ListRoomInfo[i].RoomPrice * ListUserInfo[i].DayNumber);
+
+                            Console.WriteLine("Your Name : {0} / Your Phone Number : {1} / Your Requirement : {2}\n" +
+                                "Your Reservation Day(Check In) : {3} / The nights : {4} / Your Check Out Day : {5}\nThe current time : {6}",
+                                ListUserInfo[i].UserName, ListUserInfo[i].PhoneNumber, ListUserInfo[i].Requirement,
+                                ListUserInfo[i].DayMonthYear, ListUserInfo[i].DayNumber, ListUserInfo[i].CheckoutDay, currentdaymonthyear);
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry your password is worng, please check it again Or reserve first and then make password");
+                }
+
             }
             catch (Exception e)
             {
