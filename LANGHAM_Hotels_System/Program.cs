f09771bab7969f902a43b510e4965a6174866777
jes623 <112556590+jes623@users.noreply.github.com>
2022-10-05 23:01:08 +1300

    초기 커밋.

---
+++ LANGHAM_Hotels_System/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LANGHAM_Hotels_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
